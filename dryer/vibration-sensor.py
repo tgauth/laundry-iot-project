fb6ea306a5154aa5a7bd44742dddf3fb982e8eb1
Tess Gauthier <tgauth@bu.edu>
2025-01-19 14:29:16 -0800

    Update vibration-sensor.py

--- dryer/vibration-sensor.py
+++ dryer/vibration-sensor.py
@@ -1,28 +1,6 @@
 #!/usr/bin/python
-# from https://piddlerintheroot.com/vibration-sensor/
-#import RPi.GPIO as GPIO
-#import time
+# from https://docs.sunfounder.com/projects/umsk/en/latest/05_raspberry_pi/pi_lesson24_vibration_sensor.html
 
-#GPIO SETUP
-#channel = 17
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(channel, GPIO.IN)
-
-#def callback(channel):
-#        if GPIO.input(channel):
-#                print("Movement Detected in if!")
-#        else:
-#                print("Movement Detected in else")
-
-#GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-#GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
-
-# infinite loop
-#try:
-#        while True:
-#                time.sleep(1)
-#except KeyboardInterrupt:
-#        print('Interrupted')
 from gpiozero import InputDevice
 import time
 
@@ -31,6 +9,7 @@ vibration_sensor = InputDevice(17)
 
 count = 0;
 # Continuous loop to read from the sensor
+#while True:
 while count < 120:
     # Check if the sensor is active (no vibration detected)
     if vibration_sensor.is_active:
@@ -41,3 +20,4 @@ while count < 120:
     count += 1    
     # Wait for 1 second before reading the sensor again
     time.sleep(1)
+    
