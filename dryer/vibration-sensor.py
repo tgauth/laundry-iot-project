aa7ae076365831f1d517a6227757e7f430a062c1
Tess Gauthier <tgauth@bu.edu>
2025-01-20 11:18:03 -0800

    Update vibration-sensor.py

--- dryer/vibration-sensor.py
+++ dryer/vibration-sensor.py
@@ -9,8 +9,14 @@ import time
 # Connect the digital output of the vibration sensor to GPIO17 on the Raspberry Pi
 vibration_sensor = InputDevice(17)
 
-# URL of the PHP script
-php_url = "http://rpi-laundry/request_handler.php"
+# Function to write data to a file
+def write_to_file(data, file_path='/var/www/html/dryer_data.txt'):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(data)
+            print(f"Data written to {file_path} successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 count = 0;
 # Continuous loop to read from the sensor
@@ -19,15 +25,8 @@ while count < 120:
     # Check if the sensor is active (no vibration detected)
     dryer_status = "Running" if vibration_sensor.is_active else "Not Running"
     print(f"status: {dryer_status}")
-    # Send update to the PHP script
-    data = {
-        'dryer_status': dryer_status
-    }
-    response = requests.post(php_url, data=data)
-    if response.status_code == 200:
-        print("Status update sent successfully.")
-    else:
-        print("Failed to send status update.")
+    # Call the function to write data to the file
+    write_to_file(dryer_status)
     count += 1    
     # Wait for 1 second before reading the sensor again
     time.sleep(1)
