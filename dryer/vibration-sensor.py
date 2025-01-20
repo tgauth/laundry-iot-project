04cf6af9ec9dffdc9df801df3fc6ef4cba835b17
Tess Gauthier <tgauth@bu.edu>
2025-01-20 10:41:35 -0800

    Update vibration-sensor.py

--- dryer/vibration-sensor.py
+++ dryer/vibration-sensor.py
@@ -2,22 +2,32 @@
 # from https://docs.sunfounder.com/projects/umsk/en/latest/05_raspberry_pi/pi_lesson24_vibration_sensor.html
 
 from gpiozero import InputDevice
+
+import requests
 import time
 
 # Connect the digital output of the vibration sensor to GPIO17 on the Raspberry Pi
 vibration_sensor = InputDevice(17)
+
+# URL of the PHP script
+php_url = "http://rpi-laundry/index.php"
 
 count = 0;
 # Continuous loop to read from the sensor
 #while True:
 while count < 120:
     # Check if the sensor is active (no vibration detected)
-    if vibration_sensor.is_active:
-        print("Vibration detected!")
+    dryer_status = "Running" if vibration_sensor.is_active else "Not Running"
+    print(f"status: {dryer_status}")
+    # Send update to the PHP script
+    data = {
+        'dryer_status': dryer_status
+    }
+    response = requests.post(php_url, data=data)
+    if response.status_code == 200:
+        print("Status update sent successfully.")
     else:
-        # When the sensor is inactive (vibration detected)
-        print("...")
+        print("Failed to send status update.")
     count += 1    
     # Wait for 1 second before reading the sensor again
     time.sleep(1)
-    
