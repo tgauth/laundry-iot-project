0224a2a72caf894d9328eceec6d4aee6b89b2920
Tess Gauthier <tgauth@bu.edu>
2025-01-20 11:31:47 -0800

    Create light.py

---
+++ sensor/light.py
@@ -0,0 +1,15 @@
+# from https://www.uugear.com/portfolio/using-light-sensor-module-with-raspberry-pi/
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+count = 0
+while count < 120:
+  if GPIO.input(4):
+    print("TRUE") # light off
+  else:
+    print("FALSE") # light on
+  count += 1
+  time.sleep(1)
