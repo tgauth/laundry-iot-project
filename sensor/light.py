846287ab881c119c1e0d422b76a3049b6d32eafa
Tess Gauthier <tgauth@bu.edu>
2025-01-20 11:34:56 -0800

    Update light.py

--- sensor/light.py
+++ sensor/light.py
@@ -5,11 +5,21 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
 
-count = 0
+# Function to write data to a file
+def write_to_file(data, file_path='/var/www/html/washer_data.txt'):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(data)
+            print(f"Data written to {file_path} successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+count = 0;
+# Continuous loop to read from the sensor
+#while True:
 while count < 120:
-  if GPIO.input(4):
-    print("TRUE") # light off
-  else:
-    print("FALSE") # light on
+  # light is off when input is high and light is on when input is low
+  status = "Not Running" if GPIO.input(4) else "Running":
+  write_to_file(status)
   count += 1
   time.sleep(1)
