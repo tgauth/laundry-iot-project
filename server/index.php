e1cf96b0e8aefbc7eb90b7def70e1a4b2a9e77e9
Tess Gauthier <tgauth@bu.edu>
2025-01-20 10:42:39 -0800

    Update index.php

--- server/index.php
+++ server/index.php
@@ -9,7 +9,7 @@
     <p><?php echo date('Y-m-d H:i:s'); ?></p>
 
     <h2>Washer</h2>
-    <p>Status: <span id="washerStatus"><?php echo "Coming Soon"; ?></span></p>
+    <p>Status: <span id="washerStatus"><?php echo isset($_POST['dryer_status']) ? htmlspecialchars($_POST['dryer_status']) : "I'm Broken"; ?></span></span></p>
 
     <h2>Dryer</h2>
     <p>Status: <span id="dryerStatus"><?php echo "Coming Soon"; ?></span></p>
